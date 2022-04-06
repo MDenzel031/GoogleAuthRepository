e5f0c20f779cee19b2bd1e73254a2a13bcdcd808
MDenzel031 <mdenzel31guaban@gmail.com>
2022-04-06 17:25:35 +0800

    Changing main.dart Title

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Dear Diary",
+      title: "GoogleAuthService",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Color(0xff145c9E),
